4c9f2d26d2c4423988ff192d07751b0c622c2c9b
deminsvd <demin.svd@yandex.ru>
2024-11-17 21:47:38 +0300

    Add files via upload

---
+++ HW 3 sql 1.sql
@@ -0,0 +1,114 @@
+INSERT INTO singer
+VALUES(1,'Jennifer Lopez');
+
+INSERT INTO singer
+VALUES(2,'Beyonce');
+
+INSERT INTO singer
+VALUES(3,'Jay-Z');
+
+INSERT INTO singer
+VALUES(4,'Rihanna');
+
+INSERT INTO genre
+VALUES(1,'R&B');
+
+INSERT INTO genre 
+VALUES(2,'Pop');
+
+INSERT INTO genre
+VALUES(3,'Rapping');
+
+INSERT INTO album
+VALUES(1,'Brave',2007);
+
+INSERT INTO album
+VALUES(2,'4',2011);
+
+INSERT INTO album
+VALUES(3,'The Blueprint',2020);
+
+INSERT INTO track
+VALUES(1,'Do It Well','3:05:00',1);
+
+INSERT INTO track
+VALUES(2,'Hold It Dont Drop It','3:55:00',1);
+
+INSERT INTO track
+VALUES(3,'I Miss You','2:59:00',2);
+
+INSERT INTO track
+VALUES(4,'End of Time','3:43:00',2);
+
+INSERT INTO track
+VALUES(5,'Takeover','5:13:00',3);
+
+INSERT INTO track
+VALUES(6,'My Song Cry','5:04:00',3);
+
+INSERT INTO collection
+VALUES(1,'one',2016);
+
+INSERT INTO collection
+VALUES(2,'two',2017);
+
+INSERT INTO collection
+VALUES(3,'three',2018);
+
+INSERT INTO collection
+VALUES(4,'four',2019);
+
+INSERT INTO genresinger
+VALUES(1,1);
+
+INSERT INTO genresinger
+VALUES(2,2);
+
+INSERT INTO genresinger
+VALUES(3,3);
+
+INSERT INTO genresinger
+VALUES(2,4);
+
+INSERT INTO albumsinger
+VALUES(1,1);
+
+INSERT INTO albumsinger
+VALUES(2,2);
+
+INSERT INTO albumsinger
+VALUES(3,3);
+
+INSERT INTO trackcollection
+VALUES(1,1);
+
+INSERT INTO trackcollection
+VALUES(2,1);
+
+INSERT INTO trackcollection
+VALUES(3,1);
+
+INSERT INTO trackcollection
+VALUES(3,2);
+
+INSERT INTO trackcollection
+VALUES(4,2);
+
+INSERT INTO trackcollection
+VALUES(3,3);
+
+INSERT INTO trackcollection
+VALUES(5,3);
+
+INSERT INTO trackcollection
+VALUES(6,3);
+
+INSERT INTO trackcollection
+VALUES(1,4);
+
+INSERT INTO trackcollection
+VALUES(2,4);
+
+INSERT INTO trackcollection
+VALUES(4,4);
+
